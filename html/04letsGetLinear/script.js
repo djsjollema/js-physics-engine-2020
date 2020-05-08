c4897dee67eac72cc2ec6c25839b3015ceb94554
Jelle Sjollema <=>
2020-05-08 09:23:37 +0200

    06 --finished

--- html/04letsGetLinear/script.js
+++ html/04letsGetLinear/script.js
@@ -8,27 +8,3 @@ canvas.width = width;
 canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
-
-let myLine = new LinearFunction(-0.5,300);
-
-let A = new Point(100,100,20,"orange",true);
-let B = new Point(500,200,20,"magenta",true);
-
-
-function animate(){
-  context.clearRect(0,0,width,height)
-
-  A.draw(context);
-  B.draw(context);
-
-  myLine.slope = (B.y - A.y)/(B.x -A.x);
-  // y = m*x + b
-  //y - m*x = b
-  // b = y -  m*x
-
-  myLine.intercept = B.y - myLine.slope*B.x;
-
-  myLine.draw(context,"midnightblue");
-}
-
-setInterval(animate,10);
--- html/07addingVectors/script.js
+++ html/07addingVectors/script.js
@@ -7,13 +7,4 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-
-let myPoint = new Point(200,200,50,"red",true)
-function animate(){
-  context.clearRect(0,0,width,height);
-  myPoint.draw(context)
-}
-
-
-
-setInterval(animate,10);
+// begin hier met jouw code voor deze opdracht
