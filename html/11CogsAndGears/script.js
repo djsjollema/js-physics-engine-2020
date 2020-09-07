5670195d78a86c72d3c8cd9ec32bfba2dfb7097e
Jelle Sjollema <=>
2020-09-07 14:40:38 +0200

    11 cog and gear minimum

--- html/11CogsAndGears/script.js
+++ html/11CogsAndGears/script.js
@@ -9,16 +9,23 @@ canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
 
-// create namespace
-let img = new Image();
+let img,angle;
+
+img = new Image();
 img.src = "images/cog.png";
 
-img.addEventListener('load',function(){
-    animate()
+angle = 0.1;
+
+img.addEventListener('load',()=>{
+  setInterval(animate,10)
 })
 
 function animate(){
-  requestAnimationFrame(animate)
   context.clearRect(0,0,width,height);
-  context.drawImage(img,0,0,200,200)
+  context.save();
+  context.translate(300,300);
+  context.rotate(angle);
+  context.drawImage(img,-150,-150,300,300);
+  context.restore();
+  angle += 0.01;
 }
