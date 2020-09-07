46367c2766eef9ff3028c1f02684911782f11b77
Jelle Sjollema <=>
2020-09-07 15:17:46 +0200

    11 cogs and gear afsluiting

--- html/11CogsAndGears/script.js
+++ html/11CogsAndGears/script.js
@@ -9,23 +9,31 @@ canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
 
-let img,angle;
+let img,angle,img2,x;
+
+x = 0;
 
 img = new Image();
 img.src = "images/cog.png";
 
+img2 = new Image();
+img2.src = "images/landscape.jpg"
+
 angle = 0.1;
 
-img.addEventListener('load',()=>{
+img2.addEventListener('load',()=>{
   setInterval(animate,10)
 })
 
 function animate(){
   context.clearRect(0,0,width,height);
+  context.drawImage(img2,0,0,width,height);
   context.save();
-  context.translate(300,300);
+  context.translate(x,300);
   context.rotate(angle);
+
   context.drawImage(img,-150,-150,300,300);
   context.restore();
   angle += 0.01;
+  x+=1;
 }
