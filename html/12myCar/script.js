c3aa6d82417e2784acd34501137e475d9fc1358b
djsjollema <j.sjollema@ma-web.nl>
2020-09-11 12:22:16 +0200

    12 mycar start

---
+++ html/12myCar/script.js
@@ -0,0 +1,48 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+// begin hier met jouw code voor deze opdracht
+
+let car, wheel, background;
+
+car ={};
+car.image = new Image();
+car.image.src = "images/car.png"
+car.pos = 0;
+car.speed = 2;
+
+wheel = {};
+wheel.image = new Image()
+wheel.image.src = "images/wheel.png";
+
+addEventListener('keydown',(e)=>{
+  //console.log(e.key);
+  switch(e.key) {
+    case "ArrowRight":
+      car.speed += 0.2
+      break;
+    case "ArrowLeft":
+      car.speed -= 0.2;
+      break;
+  }
+})
+
+car.image.addEventListener('load',()=>{
+  setInterval(animate,10)
+})
+
+function animate(){
+  context.clearRect(0,0,width,height);
+  context.drawImage(car.image,car.pos,0);
+
+  car.pos += car.speed;
+  if(car.pos > width){
+    car.pos = -car.image.width
+  }
+}
