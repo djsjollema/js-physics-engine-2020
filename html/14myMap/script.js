974fbbab8effb84fdfbbf2b65fd06e929b2f283c
Jelle Sjollema <=>
2020-09-21 14:23:27 +0200

    op14 less

--- html/14myMap/script.js
+++ html/14myMap/script.js
@@ -9,8 +9,31 @@ canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
 
+let img_highres, img_lowres, scale,point,coordinate;
+
+img_lowres = new Image();
+img_lowres.src = "images/map_lowres.jpg";
+
+img_highres = new Image();
+img_highres.src = "images/map_highres.jpg";
+
+scale = img_highres.width / img_lowres.width;
+coordinate = {};
+
+point = new Point(400,200,100,"white",true)
+
 setInterval(animate,10)
 
 function animate(){
+  coordinate.x = point.x - point.radius;
+  coordinate.y = point.y - point.radius;
+
   context.clearRect(0,0,width,height);
+  //drawImage(img,sx,sy,sw,sh,x,y,w,h)
+  //context.drawImage(img_highres,400,400,200,200,500,300,200,200);
+  context.drawImage(img_lowres,0,0);
+
+  //point.draw()
+  context.fillRect(coordinate.x,coordinate.y,2*point.radius,2*point.radius);
+
 }
