ccb3c9851b6308f72f657dece5c7af4bedaac58c
Jelle Sjollema <=>
2020-09-28 12:20:41 +0200

    15 my dice startcode

--- html/15myDice/script.js
+++ html/15myDice/script.js
@@ -8,22 +8,3 @@ canvas.width = width;
 canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
-
-let img,background;
-img = new Image();
-img.src = "images/nijntje.png"
-
-background = new Image();
-background.src = "images/park.png"
-
-img.addEventListener('load',()=>{
-  setInterval(animate,10)
-})
-
-
-
-function animate(){
-  context.clearRect(0,0,width,height);
-  context.drawImage(background,0,0,width,height)
-  context.drawImage(img,500,height-450)
-}
