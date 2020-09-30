b642a995ecce4af19120a7ab072a9a5f27388a5e
djsjollema <j.sjollema@ma-web.nl>
2020-09-30 13:33:28 +0200

    my dice prototype

--- html/15myDice/script.js
+++ html/15myDice/script.js
@@ -7,4 +7,58 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
+
+context.font = "80px Arial";
+
+
 // begin hier met jouw code voor deze opdracht
+
+let dice,val1,val2,score,scoreList;
+
+dice = {};
+dice.image = new Image();
+dice.image.src = "images/dice.png";
+
+scoreList = [];
+for(let i=2;i<=12;i++){
+  scoreList.push(0);
+}
+
+dice.image.addEventListener('load',()=>{
+  dice.width = dice.image.width/6;
+  dice.height = dice.image.height;
+  setInterval(animate,1000)
+
+})
+
+function animate(){
+  val1 = Math.floor(Math.random()*6)+1;
+  val2 = Math.floor(Math.random()*6)+1;
+  score = val1+val2;
+  scoreList[score-2]++
+  console.log(score,scoreList)
+
+  context.clearRect(0,0,width,height);
+  context.drawImage(
+    dice.image,
+    (val1-1)*dice.width,
+    0,
+    dice.width,
+    dice.height,
+    0,
+    0,
+    dice.width,
+    dice.height);
+
+    context.drawImage(
+      dice.image,
+      (val2-1)*dice.width,
+      0,
+      dice.width,
+      dice.height,
+      dice.width,
+      0,
+      dice.width,
+      dice.height);
+  context.fillText(score, 500, 100);
+}
