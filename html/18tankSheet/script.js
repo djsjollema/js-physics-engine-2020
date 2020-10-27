05c874d9ddefa27eb73c833158f8d7ee700e1b52
djsjollema <j.sjollema@ma-web.nl>
2020-10-27 14:57:05 +0100

    direction added

--- html/18tankSheet/script.js
+++ html/18tankSheet/script.js
@@ -19,18 +19,25 @@ spriteSheet.src = "images/tanksheet.png";
 greenTank = {};
 greenTank.animationArray = [1,2,3,4,5,6,7,8];
 greenTank.index = 0;
+greenTank.speed = 10;
 
 greenTank.x = 100;
 greenTank.y = 100;
 
 greenTank.vx = 0;
-greenTank.vy = -10;
+greenTank.vy = -greenTank.speed;
+
+greenTank.direction = 0;
 
 
 greenTank.draw = function(){
   greenTank.sx = greenTank.animationArray[greenTank.index]%8 * 84;
   greenTank.sy = Math.floor(greenTank.animationArray[greenTank.index]/8) * 84;
-  context.drawImage(spriteSheet,greenTank.sx,greenTank.sy,84,84,greenTank.x,greenTank.y,84,84)
+  context.save();
+  context.translate(greenTank.x,greenTank.y);
+  context.rotate(greenTank.direction)
+  context.drawImage(spriteSheet,greenTank.sx,greenTank.sy,84,84,-42,-42,84,84)
+  context.restore()
 }
 
 greenTank.update = function(){
@@ -39,9 +46,45 @@ greenTank.update = function(){
   if(greenTank.y <0){
     greenTank.y = height;
   }
+  if(greenTank.x > width){
+    greenTank.x = 0;
+  }
+  if(greenTank.y > height){
+    greenTank.y = 0;
+  }
+  if(greenTank.x < 0){
+    greenTank.x = width;
+  }
 }
 
+//keylistener
+document.addEventListener('keydown',(e)=>{
+  console.log(e.key)
+  switch (e.key) {
+    case "ArrowRight":
+        greenTank.vx = greenTank.speed;
+        greenTank.vy = 0;
+        greenTank.direction =0.5*Math.PI;
+      break;
+      case "ArrowDown":
+          greenTank.vx = 0;
+          greenTank.vy = greenTank.speed;
+          greenTank.direction =Math.PI;
+        break;
+        case "ArrowLeft":
+            greenTank.vx = -greenTank.speed;
+            greenTank.vy = 0;
+            greenTank.direction =1.5 * Math.PI;
+          break;
+          case "ArrowUp":
+              greenTank.vx = 0;
+              greenTank.vy = -greenTank.speed;
+              greenTank.direction =0;
+            break;
+    default:
 
+  }
+})
 
 spriteSheet.addEventListener('load',()=>{
   sw = spriteSheet.width/8;
