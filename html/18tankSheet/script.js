62c50994d09d36c423c09b9993a03ec45c538d62
Jelle Sjollema <=>
2020-10-30 12:05:17 +0100

    18 tank direction added

--- html/18tankSheet/script.js
+++ html/18tankSheet/script.js
@@ -19,6 +19,7 @@ spriteSheet.src = "images/tanksheet.png";
 greenTank = {};
 greenTank.animationArray = [8,7,6,5,4,3,2,1];
 greenTank.index = 0;
+greenTank.direction = 0;
 
 greenTank.x = 100;
 greenTank.y = 100;
@@ -30,7 +31,12 @@ greenTank.vy = -10;
 greenTank.draw = function(){
   greenTank.sx = greenTank.animationArray[greenTank.index]%8 * 84;
   greenTank.sy = Math.floor(greenTank.animationArray[greenTank.index]/8) * 84;
-  context.drawImage(spriteSheet,greenTank.sx,greenTank.sy,84,84,greenTank.x,greenTank.y,84,84)
+  context.save();
+  context.translate(greenTank.x,greenTank.y)
+  context.rotate(greenTank.direction)
+
+  context.drawImage(spriteSheet,greenTank.sx,greenTank.sy,84,84,-42,-42,84,84);
+  context.restore();
 }
 
 greenTank.update = function(){
@@ -39,12 +45,42 @@ greenTank.update = function(){
   if(greenTank.y <0){
     greenTank.y = height;
   }
+  if(greenTank.x <0){
+    greenTank.x = width;
+  }
+  if(greenTank.y > height){
+    greenTank.y = 0;
+  }
+  if(greenTank.x > width){
+    greenTank.x = 0;
+  }
 }
 
 window.addEventListener('keydown',(e)=>{
-  console.log(e.key)
+  switch (e.key) {
+    case "ArrowRight":
+        greenTank.vx = 10;
+        greenTank.vy = 0;
+        greenTank.direction = 0.5 * Math.PI;
+      break;
+      case "ArrowDown":
+          greenTank.vx = 0;
+          greenTank.vy = 10;
+          greenTank.direction = Math.PI;
+      break;
+      case "ArrowLeft":
+          greenTank.vx = -10;
+          greenTank.vy = 0;
+          greenTank.direction = 1.5 * Math.PI;
+      break;
+      case "ArrowUp":
+          greenTank.vx = 0;
+          greenTank.vy = -10;
+          greenTank.direction = 0;
+      break;
+    default:
+  }
 })
-
 
 
 spriteSheet.addEventListener('load',()=>{
