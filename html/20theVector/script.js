87a8be83092a7d981f96602715bccbe34979d4d7
djsjollema <j.sjollema@ma-web.nl>
2020-12-11 11:57:35 +0100

    20 sumVector 2

--- html/20theVector/script.js
+++ html/20theVector/script.js
@@ -10,11 +10,12 @@ canvas.height = height;
 // begin hier met jouw code voor deze opdracht
 
 // declare namespace
-let pointA, positionVectorA, poitB, positionVectorB;
+let pointA, positionVectorA, pointB, positionVectorB,sumVector;
 
 // assign values
 positionVectorA = new Vector2d(300,200);
 positionVectorB = new Vector2d(100,300);
+sumVector = new Vector2d(0,0)
 
 pointA = new Point(100,100,20,"rgba(255,255,30,0.5)",true,"pointA");
 pointB = new Point(100,300,20,"rgba(255,255,30,0.5)",true,"pointB");
@@ -24,9 +25,13 @@ animate();
 //animation loop
 function animate(){
   requestAnimationFrame(animate);
+  sumVector.sumVector(pointA.position,pointB.position)
+
   context.clearRect(0,0,width,height);
   pointA.position.draw(0,0,"blue");
   pointB.position.draw(0,0,"red");
+  pointB.position.draw(pointA.position.dx,pointA.position.dy,"red");
+  sumVector.draw(0,0,"yellow")
   pointA.draw();
   pointB.draw();
 
