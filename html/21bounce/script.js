134a6f07a6c21682655dbd42b82548fc70bc9391
djsjollema <j.sjollema@ma-web.nl>
2021-01-08 11:57:15 +0100

    21 bounce

---
+++ html/21bounce/script.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+// begin hier met jouw code voor deze opdracht
+
+// declare namespace
+
+
+// assign values
+
+
+animate();
+
+//animation loop
+function animate(){
+
+
+}
