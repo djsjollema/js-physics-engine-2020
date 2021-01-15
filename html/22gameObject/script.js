788570df20d4c23f010d88e839426ec7e8a57916
djsjollema <j.sjollema@ma-web.nl>
2021-01-15 11:52:22 +0100

    22

--- html/22gameObject/script.js
+++ html/22gameObject/script.js
@@ -7,17 +7,21 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-// begin hier met jouw code voor deze opdracht
+// begin hier met jouw code voor deze opdrachtlet myGO;
 
 // declare namespace en scope
+let myGO;
+
 
 // assign values
-
+myGO = new GameObject(new Vector2d(200,200),new Vector2d(4,5),new Vector2d(0,0));
 
 animate();
 
 //animation loop
 function animate(){
   context.clearRect(0,0,width,height)
-  requestAnimationFrame(animate)
+  requestAnimationFrame(animate);
+  myGO.update();
+  myGO.draw(context);
 }
