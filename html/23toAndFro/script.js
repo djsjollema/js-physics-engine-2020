8fa81fa37a8111bd60626f1774ba031f05864046
djsjollema <j.sjollema@ma-web.nl>
2021-01-22 12:01:55 +0100

    23 difVector --add

--- html/23toAndFro/script.js
+++ html/23toAndFro/script.js
@@ -8,19 +8,30 @@ canvas.width = width;
 canvas.height = height;
 
 // begin hier met jouw code voor deze opdrachtlet myGO;
+let A,B, point,dif;
 
 // declare namespace en scope
-// let myGO;
-
-// assign values
-
-//myGO = new GameObject(new Vector2d(200,200),new Vector2d(4,5),new Vector2d(0,1));
-
-//console.log( getRandomInt(10, 100))
+A = new GameObject(new Vector2d(100,100),new Vector2d(0,0),new Vector2d(0,0));
+B = new GameObject(new Vector2d(400,300),new Vector2d(0,0),new Vector2d(0,0));
+point = new GameObject(new Vector2d(200,200),new Vector2d(-1,1),new Vector2d(0,0));
+//point.vel.magnitude = 2;
+dif = new Vector2d(0,0)
 animate();
 
 //animation loop
 function animate(){
   context.clearRect(0,0,width,height)
   requestAnimationFrame(animate);
+  A.draw(context);
+  B.draw(context);
+
+  dif.difVector(B.pos,point.pos);
+  B.pos.draw(0,0,"white",1);
+  point.pos.draw(0,0,"white",1);
+
+
+  point.draw(context);
+  point.vel.draw(point.pos.dx,point.pos.dy,"red",20)
+  dif.draw(point.pos.dx,point.pos.dy,"blue",1)
+  point.update();
 }
