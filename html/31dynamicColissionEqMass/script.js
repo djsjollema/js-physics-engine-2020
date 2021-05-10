f1854c29e246113250f19f1898597520aaad3d5e
djsjollema <j.sjollema@ma-web.nl>
2021-05-10 14:59:33 +0200

    31 line

--- html/31dynamicColissionEqMass/script.js
+++ html/31dynamicColissionEqMass/script.js
@@ -8,31 +8,55 @@ canvas.width = width;
 canvas.height = height;
 
 // declare namespace en scope
-let player,points,numberOfPoints;
+let GO_a, GO_b,dist,collision,line_ab;
 
-points = [];
+GO_a = new GameObject(new Vector2d(200,200), new Vector2d(2,2), new Vector2d(0,0));
+GO_b = new GameObject(new Vector2d(800,300), new Vector2d(-2,-2), new Vector2d(0,0));
+line_ab = new LinearFunction(1,0)
 
-numberOfPoints = 20;
-for(let i=0; i<numberOfPoints; i++){
-  let point = new Point(getRandomInt(0, width),getRandomInt(0, height),getRandomInt(50, 100),"black",false,"");
-  points.push(point);
-}
+GO_a.radius = 75;
+GO_b.radius = 75;
+GO_a.color = "rgba(255,0,0,0.4)"
+GO_b.color = "rgba(0,0,255,0.4)"
 
-player = new GameObject(new Vector2d(getRandomInt(0, width),getRandomInt(0, height)),new Vector2d(2,3),new Vector2d(0,0));
+distance = Infinity;
+collision = false;
 
+animate();
 
-anime()
-
-function anime(){
-  requestAnimationFrame(anime);
+function animate(){
+  requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
-  points.map((point,i)=>{
-    point.draw(context);
-    checkColissions(point)
-  })
-  player.draw(context)
+
+  dist = GO_b.distanceTo(GO_a);
+  if(dist < GO_a.radius + GO_b.radius){
+    collision = true;
+    GO_a.color = "yellow";
+    GO_b.color = "orange";
+  } else {
+    collision = false;
+    GO_a.color = "rgba(255,0,0,0.4)";
+    GO_b.color = "rgba(0,0,255,0.4)";
+  }
+
+  
+  drawLine(line_ab,GO_a.pos,GO_b.pos);
+  line_ab.draw(context,"blue");
+
+  GO_a.update();
+  GO_b.update();
+
+  GO_a.vel.draw(GO_a.pos.dx,GO_a.pos.dy,"rgba(100,100,100,0.4)",70);
+  GO_b.vel.draw(GO_b.pos.dx,GO_b.pos.dy,"rgba(100,100,100,0.4)",70);
+  
+  GO_a.draw(context);
+  GO_b.draw(context);
 }
 
-function checkColissions(point){
-  if(player.pos.)
+function drawLine(line_ab,a,b){
+  let dx = b.dx - a.dx;
+  let dy = b.dy - a.dy;
+  line_ab.slope = dy/dx;
+  line_ab.intercept = a.dy - line_ab.slope * a.dx;
+
 }
